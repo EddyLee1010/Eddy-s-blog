ff91149390adaba2e0d666c8d07a70aca5502c3f
李子强 <120054642@qq.com>
2018-03-30 14:59:26 +0800

    正式实战

--- server/http_server.php
+++ server/http_server.php
@@ -14,18 +14,7 @@ $http->set(
     ]
 );
 $http->on('request', function($request, $response) {
-    //print_r($request->get);
-    $content = [
-        'date:' => date("Ymd H:i:s"),
-        'get:' => $request->get,
-        'post:' => $request->post,
-        'header:' => $request->header,
-    ];
 
-    swoole_async_writefile(__DIR__."/access.log", json_encode($content).PHP_EOL, function($filename){
-        // todo
-    }, FILE_APPEND);
-    $response->cookie("singwa", "xsssss", time() + 1800);
     $response->end("sss". json_encode($request->get));
 });
 
