9117494c498f8364e610759c709d3f0cf978848c
李子强 <120054642@qq.com>
2018-03-30 14:55:35 +0800

    正式实战

--- server/http_server.php
+++ server/http_server.php
@@ -1,10 +1,32 @@
 <?php
-$http = new swoole_http_server("0.0.0.0", 9501);
+/**
+ * Created by PhpStorm.
+ * @auth:Eddy
+ * @527084800@qq.com
+ * Date: 2018/3/30 0030
+ */
+$http = new swoole_http_server("0.0.0.0", 8811);
 
-$http->set([
-	'enable_static_handler' => true,
-	'document_root' => __DIR__ ]); 
-$http->on('request', function ($request, $response) {
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>"); 
-}); 
+$http->set(
+    [
+        'enable_static_handler' => true,
+        'document_root' => "/usr/share/nginx/html/blog/public/static",
+    ]
+);
+$http->on('request', function($request, $response) {
+    //print_r($request->get);
+    $content = [
+        'date:' => date("Ymd H:i:s"),
+        'get:' => $request->get,
+        'post:' => $request->post,
+        'header:' => $request->header,
+    ];
+
+    swoole_async_writefile(__DIR__."/access.log", json_encode($content).PHP_EOL, function($filename){
+        // todo
+    }, FILE_APPEND);
+    $response->cookie("singwa", "xsssss", time() + 1800);
+    $response->end("sss". json_encode($request->get));
+});
+
 $http->start();
