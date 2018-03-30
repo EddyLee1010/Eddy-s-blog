1154a86f3dca4a6abdabbd717bc318603ffa8425
李子强 <120054642@qq.com>
2018-03-30 13:59:36 +0800

    1

--- server/websocket.php
+++ server/websocket.php
@@ -5,19 +5,97 @@
  * @527084800@qq.com
  * Date: 2018/3/30 0030
  */
-$server = new swoole_websocket_server("0.0.0.0", 9501);
+class Ws {
 
-$server->on('open', function (swoole_websocket_server $server, $request) {
-    echo "server: handshake success with fd{$request->fd}\n";
-});
+    CONST HOST = "0.0.0.0";
+    CONST PORT = 9501;
 
-$server->on('message', function (swoole_websocket_server $server, $frame) {
-    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-    $server->push($frame->fd, "this is server");
-});
+    public $ws = null;
+    public function __construct() {
+        $this->ws = new swoole_websocket_server("0.0.0.0", 8812);
 
-$server->on('close', function ($ser, $fd) {
-    echo "client {$fd} closed\n";
-});
+        $this->ws->set(
+            [
+                'worker_num' => 2,
+                'task_worker_num' => 2,
+            ]
+        );
+        $this->ws->on("open", [$this, 'onOpen']);
+        $this->ws->on("message", [$this, 'onMessage']);
+        $this->ws->on("task", [$this, 'onTask']);
+        $this->ws->on("finish", [$this, 'onFinish']);
+        $this->ws->on("close", [$this, 'onClose']);
 
-$server->start();
+        $this->ws->start();
+    }
+
+    /**
+     * 监听ws连接事件
+     * @param $ws
+     * @param $request
+     */
+    public function onOpen($ws, $request) {
+        var_dump($request->fd);
+        if($request->fd == 1) {
+            // 每2秒执行
+            swoole_timer_tick(2000, function($timer_id){
+                echo "2s: timerId:{$timer_id}\n";
+            });
+        }
+    }
+
+    /**
+     * 监听ws消息事件
+     * @param $ws
+     * @param $frame
+     */
+    public function onMessage($ws, $frame) {
+        echo "ser-push-message:{$frame->data}\n";
+        // todo 10s
+        $data = [
+            'task' => 1,
+            'fd' => $frame->fd,
+        ];
+        //$ws->task($data);
+
+        swoole_timer_after(5000, function() use($ws, $frame) {
+            echo "5s-after\n";
+            $ws->push($frame->fd, "server-time-after:");
+        });
+        $ws->push($frame->fd, "server-push:".date("Y-m-d H:i:s"));
+    }
+
+    /**
+     * @param $serv
+     * @param $taskId
+     * @param $workerId
+     * @param $data
+     */
+    public function onTask($serv, $taskId, $workerId, $data) {
+        print_r($data);
+        // 耗时场景 10s
+        sleep(10);
+        return "on task finish"; // 告诉worker
+    }
+
+    /**
+     * @param $serv
+     * @param $taskId
+     * @param $data
+     */
+    public function onFinish($serv, $taskId, $data) {
+        echo "taskId:{$taskId}\n";
+        echo "finish-data-sucess:{$data}\n";
+    }
+
+    /**
+     * close
+     * @param $ws
+     * @param $fd
+     */
+    public function onClose($ws, $fd) {
+        echo "clientid:{$fd}\n";
+    }
+}
+
+$obj = new Ws();
