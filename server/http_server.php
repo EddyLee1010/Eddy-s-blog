27592b7181ec0486fd6b5c5b7875c0c8edc9ddb5
Eddy <527084800@qq.com>
2018-03-30 11:17:00 +0800

    ali commit

--- server/http_server.php
+++ server/http_server.php
@@ -1,20 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: baidu
- * Date: 18/2/28
- * Time: 上午1:39
- */
-$http = new swoole_http_server("0.0.0.0", 8811);
+$http = new swoole_http_server("0.0.0.0", 9501);
 
-$http->set(
-    [
-        'enable_static_handler' => true,
-        'document_root' =>"/usr/share/nginx/html/Eddy-s-blog/public/static",
-    ]
-);
-$http->on('request', function($request, $response) {
-
-});
-
+$http->set([
+	'enable_static_handler' => true,
+	'document_root' => __DIR__ ]); 
+$http->on('request', function ($request, $response) {
+    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>"); 
+}); 
 $http->start();
